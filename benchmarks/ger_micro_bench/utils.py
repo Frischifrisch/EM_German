299906f037e7667ebb3cff9185b8701ea122f5f2
Sourcery AI <>
2024-01-09 06:28:56 +0000

    'Refactored by Sourcery'

--- benchmarks/ger_micro_bench/utils.py
+++ benchmarks/ger_micro_bench/utils.py
@@ -3,20 +3,20 @@ import random
 
 def process_docs(dataset: datasets.Dataset):
     def _helper(doc):
-      # modifies the contents of a single
-      # document in our dataset.
-      
-      possible_answers=['A', 'B', 'C', 'D', 'E']
+        # modifies the contents of a single
+        # document in our dataset.
 
-      random.shuffle(possible_answers)
-      distractor_no=0
-      while possible_answers:
-        distractor = possible_answers.pop(0)
-        if distractor == doc["output"]:
-          continue
-        else:
-           distractor_no+=1
-           doc['distractor'+str(distractor_no)] = distractor
-      assert distractor_no == 4
-      return doc
+        possible_answers=['A', 'B', 'C', 'D', 'E']
+
+        random.shuffle(possible_answers)
+        distractor_no=0
+        while possible_answers:
+            distractor = possible_answers.pop(0)
+            if distractor == doc["output"]:
+                continue
+            distractor_no+=1
+            doc[f'distractor{distractor_no}'] = distractor
+        assert distractor_no == 4
+        return doc
+
     return dataset.map(_helper) # returns back a datasets.Dataset object
